59483bc01348ce9743e90010be7909816f3bae64
TaiYun712 <wendy0712000@gmail.com>
2025-06-13 06:33:15 +0800

    要放東西在手上，手部追蹤可能會跑掉

---
+++ Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseDemo : MonoBehaviour
+{
+    public GameObject infoPanel;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
