6e34ce3bee8cb2cc1530902150499b6aa12d20c7
TaiYun712 <wendy0712000@gmail.com>
2025-06-13 10:33:43 +0800

    右手完成:生長、發射能量球

--- Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
+++ Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,28 @@ using UnityEngine.UI;
 public class PoseDemo : MonoBehaviour
 {
     public GameObject infoPanel;
+    public GameObject tablePanel;
+
     public Text skillNameText;
 
     public int currentSkill = 0;
     public int totalSkill = 4;
+
+    public GameObject ballPf;
+    public float spawnSpeed = 5;
+    public Transform shootPos;
+
+    public bool isShooting;
+   
+
     void Start()
     {
         infoPanel.SetActive(false);
+        tablePanel.SetActive(false);
         currentSkill = 0;
+
+        isShooting = false;
+        
     }
 
     //資訊面板
@@ -27,35 +42,69 @@ public class PoseDemo : MonoBehaviour
         infoPanel.SetActive(false);
     }
 
-   //切換屬性
-   public void SwitchToNextSkill()
-   {
-       currentSkill = (currentSkill + 1) % totalSkill;
+    //切換屬性
+    public void SwitchToNextSkill()
+    {
+        currentSkill = (currentSkill + 1) % totalSkill;
 
-       switch (currentSkill)
-       {
-           case 0:
-               skillNameText.text = "無屬性";
-               break;
-           
-           case 1:
-               skillNameText.text = "淨化";
+        switch (currentSkill)
+        {
+            case 0:
+                skillNameText.text = "無屬性";
+                break;
 
-               break;
-           
-           case 2:
-               skillNameText.text = "生長";
+            case 1:
+                skillNameText.text = "淨化";
 
-               break;
-           
-           case 3:
-               skillNameText.text = "移除";
+                break;
 
-               break;
-       }
-   }
-   
-   
-    
-    
+            case 2:
+                skillNameText.text = "生長";
+
+                break;
+
+            case 3:
+                skillNameText.text = "移除";
+
+                break;
+        }
+    }
+
+    //濕地合成台
+    public void OpenTablePanel()
+    {
+        tablePanel.SetActive(true);
+    }
+
+    public void CloseTablePanel()
+    {
+        tablePanel.SetActive(false);
+
+    }
+
+    //發射能量球
+
+    public void HoldRock()
+    {
+        isShooting = true;
+    }
+
+    public void ShootTheBall()
+    {
+        if (isShooting)
+        {
+            Vector3 bulletPos = shootPos.transform.position;
+            
+            GameObject shootBall = Instantiate(ballPf, bulletPos, Quaternion.identity);
+            Rigidbody shootBallRB = shootBall.GetComponent<Rigidbody>();
+            shootBallRB.velocity = transform.forward * spawnSpeed;
+            isShooting = false;
+        }
+       
+    }
+
+  
+
+
+
 }
