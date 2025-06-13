8bd4b9a2bbeb7dd79c345d5c4e88f1f4223ebf81
TaiYun712 <wendy0712000@gmail.com>
2025-06-13 08:23:25 +0800

    完成左手動作

--- Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
+++ Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PoseDemo : MonoBehaviour
 {
     public GameObject infoPanel;
-    
+    public Text skillNameText;
+
+    public int currentSkill = 0;
+    public int totalSkill = 4;
     void Start()
     {
-        
+        infoPanel.SetActive(false);
+        currentSkill = 0;
     }
 
-    void Update()
+    //資訊面板
+    public void OpenInfoPanel()
     {
-        
+        infoPanel.SetActive(true);
     }
+
+    public void CloseInfoPanel()
+    {
+        infoPanel.SetActive(false);
+    }
+
+   //切換屬性
+   public void SwitchToNextSkill()
+   {
+       currentSkill = (currentSkill + 1) % totalSkill;
+
+       switch (currentSkill)
+       {
+           case 0:
+               skillNameText.text = "無屬性";
+               break;
+           
+           case 1:
+               skillNameText.text = "淨化";
+
+               break;
+           
+           case 2:
+               skillNameText.text = "生長";
+
+               break;
+           
+           case 3:
+               skillNameText.text = "移除";
+
+               break;
+       }
+   }
+   
+   
+    
+    
 }
