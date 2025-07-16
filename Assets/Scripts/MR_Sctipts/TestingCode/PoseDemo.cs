487bb95b7a43fc727bf38f802c0ab42fa5445bd7
TaiYun712 <wendy0712000@gmail.com>
2025-07-16 18:04:30 +0800

    完成準心

--- Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
+++ Assets/Scripts/MR_Sctipts/TestingCode/PoseDemo.cs
@@ -19,9 +19,12 @@ public class PoseDemo : MonoBehaviour
 
     public GameObject ballPf;
     public GameObject aimLine;
+    public GameObject aimImpactPf;
     public float spawnSpeed = 5;
     public float ballDestoryTime = 5f; 
     public Transform shootPos;
+    public float maxLineDistance = 5f;
+    public LayerMask layerMask;
 
     public bool isShooting;
    
@@ -37,7 +40,15 @@ public class PoseDemo : MonoBehaviour
         currentSkill = 0;
 
         isShooting = false;
-        
+        aimImpactPf.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isShooting)
+        {
+            HoldToAim();
+        }
     }
 
     //比中指bad bad
@@ -114,8 +125,28 @@ public class PoseDemo : MonoBehaviour
     {
         isShooting = true;
         aimLine.SetActive(true);
-        
         AudioManager.instance.AimReadySound();
+    }
+
+    public void HoldToAim()
+    {
+        Ray ray = new Ray(shootPos.position, shootPos.forward);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, layerMask);
+
+        Vector3 endPos = Vector3.zero;
+        if (hasHit)
+        {
+            endPos = hit.point;
+            aimImpactPf.SetActive(true);
+
+            Quaternion aimImpactRotate = Quaternion.LookRotation(-hit.normal);
+            aimImpactPf.transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            aimImpactPf.transform.rotation = aimImpactRotate;
+        }
+        else
+        {
+            endPos = shootPos.position + shootPos.forward * maxLineDistance;
+        }
     }
 
     public void ShootTheBall()
