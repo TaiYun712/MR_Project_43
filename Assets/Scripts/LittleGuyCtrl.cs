358f227760380d6c6ee77217daa1672b2ef6b280
TaiYun712 <wendy0712000@gmail.com>
2025-12-31 10:39:34 +0800

    加一個暴力對話UI

--- Assets/Scripts/LittleGuyCtrl.cs
+++ Assets/Scripts/LittleGuyCtrl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,40 @@ public class LittleGuyCtrl : MonoBehaviour
 {
     public Transform leftHandPos;
     public Transform rightHandPos;
+
+    public Transform outSidePos;
     
     public Animator guyAni;
 
     public Transform initialPos;
     public ParticleSystem shoeUpPrt;
+
+    public GameObject temDiolog;
     
     void Start()
     {
         transform.position = initialPos.transform.position;
-
-    }
-
-    void Update()
-    {
         
+        temDiolog.SetActive(false);
     }
-    
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("maphouse"))
+        {
+            temDiolog.SetActive(true);
+            
+            Invoke("CloseDiolog",2f);
+        }
+    }
+
+    void CloseDiolog()
+    {
+        temDiolog.SetActive(false);
+    }
+
+
     public void HoldGuy()
     {
         guyAni.SetTrigger("isfine");
@@ -30,19 +48,19 @@ public class LittleGuyCtrl : MonoBehaviour
 
     public void CallBackGuy_L()
     {
-        transform.position = leftHandPos.position;
-        
-        Vector3 currentEulerAngles = transform.eulerAngles;
-        transform.rotation = Quaternion.Euler(0f, currentEulerAngles.y, 0f);
-        
-        guyAni.SetTrigger("showup");
-        shoeUpPrt.Play();
-        AudioManager.instance.ShowHint();
+       GuyShowUp(leftHandPos);
     }
 
     public void CallBackGuy_R()
     {
-        transform.position = rightHandPos.position;
+        GuyShowUp(rightHandPos);
+    }
+
+    public void GuyShowUp(Transform herePos)
+    {
+        transform.SetParent(outSidePos);
+        transform.position = herePos.position;
+        gameObject.SetActive(true);
         
         Vector3 currentEulerAngles = transform.eulerAngles;
         transform.rotation = Quaternion.Euler(0f, currentEulerAngles.y, 0f);
