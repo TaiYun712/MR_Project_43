e2c0a85aae6f1c4df22653caa26fffef23af657d
TaiYun712 <wendy0712000@gmail.com>
2026-04-06 04:12:59 +0800

    完成"關卡選取場景"Hold手勢UI設定

--- Assets/Scripts/Level Selection.cs
+++ Assets/Scripts/Level Selection.cs
@@ -1,21 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelection : MonoBehaviour
 {
     public Transform seePoint;
     public GameObject hintsUI;
-    
     public GameObject changeSceneEffect;
+
+    public float holdTime = 3.0f;
+    private float countdownTime;
+
+    [Header("一般關卡 手勢UI")] 
+    public Image yaHint;
+    public bool isYA = false;
+
+    [Header("教學關卡 手勢UI")] 
+    public Image goodHint;
+    public bool isGood = false;
     
     void Start()
     {
         Invoke("SetHintsHeight",0.5f);
-        
         changeSceneEffect.SetActive(false);
         AudioManager.instance.BubleUp();
+
+        countdownTime = holdTime;
+        yaHint.fillAmount = 1;
+        goodHint.fillAmount = 1;
     }
 
     public void SetHintsHeight()
@@ -24,6 +39,75 @@ public class LevelSelection : MonoBehaviour
         Vector3 targetPos = new Vector3(eyePos.x, eyePos.y-0.5f, eyePos.z); // Y 是高度, Z 是距離前方
         hintsUI.transform.position = targetPos;
     }
+
+    private void Update()
+    {
+       if(!isYA && !isGood){return;}
+
+       if (isYA)
+       {
+           HoldingGesture(yaHint);
+       }
+      
+
+       if (isGood)
+       {
+           HoldingGesture(goodHint);
+       }
+    }
+
+    void HoldingGesture(Image hintImage)
+    {
+        if (countdownTime > 0)
+        {
+            countdownTime -= Time.deltaTime;
+            hintImage.fillAmount = countdownTime / holdTime;
+        }
+        else
+        {
+            if (isYA)
+            {
+                GoToNormalLeval();
+            }
+            else if(isGood)
+            {
+                GoToTeachingLeval();
+            }
+        }
+    }
+
+    // YA 手勢觸發
+    public void HoldingYA()
+    {
+        if(isGood){return;}
+        
+        AudioManager.instance.PutPlantInToCraft();
+        isYA = true;
+        isGood = false;
+    }
+    
+    //  Good 手勢觸發
+    public void HoldingGood()
+    {
+        if(isYA){return;}
+        
+        AudioManager.instance.PutPlantInToCraft();
+        isGood = true;
+        isYA = false;
+    }
+    
+    //關閉所有觸發
+    public void HoldingNothing()
+    {
+        isYA = false;
+        isGood = false;
+
+        countdownTime = holdTime;
+        yaHint.fillAmount = 1;
+        goodHint.fillAmount = 1;
+    }
+    
+    
     
     //進入一般關卡
     public void GoToNormalLeval()
